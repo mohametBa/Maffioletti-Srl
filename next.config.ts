993dd786dd85716320cc9328bbbbe439319ec0e8
Mohamet Ba <metzooba@gmail.com>
2026-02-12 15:42:47 +0100

    next

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // Export statique
+  distDir: 'out',
+  images: {
+    unoptimized: true // Nécessaire pour export statique
+  }
 }
 
 module.exports = nextConfig
